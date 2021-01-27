0c48dc17c88249db0526f24ff0dcb4f5c751f738
Jeongik Cha <jeongik@google.com>
2021-01-28 00:49:48 +0900

    power: Specify version for aidl_interface explicitly
    Bug: 150578172
    Test: m
    Change-Id: I4a9bf218b92252403e9ebbe8f89b10ee1899283f

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
@@ -12,8 +13,7 @@ LOCAL_SHARED_LIBRARIES := \
     libdl \
     libbase \
     libutils \
-    android.hardware.power-ndk_platform \
-    vendor.lineage.power-ndk_platform \
+    android.hardware.power-V1-ndk_platform \
     libbinder_ndk
 
 LOCAL_HEADER_LIBRARIES := \
